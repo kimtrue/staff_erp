79c09ea8062123ed14d15946e1e81897d1e6cdad
trueekim <kim_truee@naver.com>
2020-04-09 19:44:44 +0900

    mail 인증 삭제

--- staff/src/main/java/kr/co/staff/main/controller/MailController.java
+++ staff/src/main/java/kr/co/staff/main/controller/MailController.java
@@ -128,7 +128,7 @@ public class MailController {
 	@RequestMapping("/sendmail.do")
 	@ResponseBody
 	public String mailSending(HttpServletRequest req, Mail mail) {
-		String setfrom = "kjs3597@gmail.com";
+		String setfrom = "";
 		String tomail = mail.getMailTo();
 		String title = mail.getMailSubject();// 제목
 		String content = mail.getEditordata();// 내용
